c2b722e3c8fd06c4fce0fc3455a6b6894bf0c1ca
Jack Wilson <wilso313@wwu.edu>
2020-05-08 10:48:29 -0700

    adheres to lab specs

--- Lab_2/Lab_2.ino
+++ Lab_2/Lab_2.ino
@@ -1,20 +1,35 @@
 
 // Created by Jack Wilson 5/5/2020 for EE 352 lab 2
-// The function of this program is to convert a serial integer input
-//  into greycode.
+// Last updated 5/8/2020 to adhere to the lab specifications
+// The function of this program is to convert a serial binary character input
+//  into greycode and count until 8 bits are filled (255).
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  int aByte;
+  // check for serial input
   if (Serial.available()) {
-    aByte = Serial.parseInt();
-    Serial.println(BinToGrey(aByte), BIN);
+    int aByte = Serial.read();
+
+    // report all conversions up to 255
+    while (aByte < 256) {
+      Serial.print("Decimal: " + (String)aByte + ", Binary: ");
+      Serial.print(aByte, BIN);
+      Serial.print(", Grey: ");
+      Serial.print(BinToGrey(aByte), BIN);
+      Serial.println();
+      aByte += 1;
+    }
+
+    while (true) {
+      // lock/finish the program
+    }
   }
 }
 
+// converts a binary input into grey code
 byte BinToGrey(byte aByte) {
   return aByte ^ (aByte >> 1);
 }
